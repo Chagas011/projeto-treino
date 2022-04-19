784656fcf595f77c8bae0db40db98107515784a9
Chagas011 <washington.chagas.9@hotmail.com>
2022-04-19 20:12:44 -0300

    search

--- home/urls.py
+++ home/urls.py
@@ -5,6 +5,7 @@ from . import views
 app_name = 'polls'
 urlpatterns = [
     path('', views.home, name='home'),
+    path('home/search/', views.search, name='search'),
     path('home/video/<int:id>/', views.video, name='v'),
     path('home/categoria/<int:categoria_id>/',
          views.categoria, name='categoria'),
--- home/views.py
+++ home/views.py
@@ -1,3 +1,5 @@
+from django.db.models import Q
+from django.http import Http404
 from django.shortcuts import get_list_or_404, get_object_or_404, render
 
 from .models import Video
@@ -27,4 +29,23 @@ def categoria(request, categoria_id):
 
     return render(request, 'home/pages/categoria.html', context={
         'videos': categoria
+    })
+
+
+def search(request):
+    search_term = request.GET.get('search', '').strip()
+    if not search_term:
+        raise Http404()
+
+    videos = Video.objects.filter(
+        Q(
+            Q(titulo__icontains=search_term) |
+            Q(descricao__icontains=search_term),
+        ),
+        publicado=True
+    ).order_by('-id')
+
+    return render(request, 'home/pages/search.html', context={
+        'search_term': search_term,
+        'videos': videos
     })
